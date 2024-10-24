9b7838add7e2be6b7fb909a101950018bbe6a29f
Damian Suidgeest <damiansuidgeest@gmail.com>
2024-10-24 00:13:56 -0700

    Use the gradient instead of repeating the objective function value

--- model/mnist_hinge.c
+++ model/mnist_hinge.c
@@ -8,34 +8,46 @@
 #include <string.h>
 #include <math.h>
 
+#define FILEPATH_X_OFFSET 25
+
 void init() {
 	float data[784];
 	char filepath[] = "data/mnist_hinge/weights_x.csv";
+	srand(42);
 	for (int i = 0; i < 10; i++) {
 		for (int j = 0; j < 784; j++) {
-			data[j] = (float)rand()/(float)(RAND_MAX) - 0.5;
+			data[j] = (float)rand()/(10 * (float)(RAND_MAX)) - 0.05;
 		}
-		filepath[23] = i + '0'; // Replace 'x' with i in the above string
+		filepath[FILEPATH_X_OFFSET] = i + '0'; // Replace 'x' with i in the above string
 		write_csv_contents(filepath, data, 1, 784);
 	}
 }
 
-void train(int iterations, float learn_rate, int num) {
+float norm(struct Matrix m) {
+	float total = 0.0f;
+	for (int i = 0; i < m.rows * m.cols; i++) {
+		total += m.data[i] * m.data[i];
+	}
+	return sqrt(total);
+}
+
+void train(int iterations, float learn_rate) {
 	char filepath[] = "data/mnist_hinge/weights_x.csv";
 	struct Matrix input_nodes = { 784, 1, 0};
 	struct Matrix ensemble_weights[10];
 	for (int i = 0; i < 10; i++) {
-		filepath[23] = i + '0';
+		filepath[FILEPATH_X_OFFSET] = i + '0';
 		struct Matrix m = { 1, 784, read_csv_contents(filepath) }; // Stored as 1 x 784 for multiplication later
 		ensemble_weights[i] = m;
 	}
 
 	struct MnistCSV training_data = {
 		fopen("data/mnist/mnist_train.csv", "r"),
-		malloc(785 * num * sizeof(float))
+		malloc(785 * sizeof(float))
 	};
 	int num_correct = 0;
-	int num_training_examples = count_num_lines(training_data.file);
+	// int num_training_examples = count_num_lines(training_data.file);
+	int num_training_examples = 5000; //TODO undo
 	rewind(training_data.file);
 
 	struct Matrix gradients[10];
@@ -46,9 +58,7 @@ void train(int iterations, float learn_rate, int num) {
 	for (int i = 0; i < iterations; i++) {
 		// Reset the gradients
 		for (int j = 0; j < 10; j++) {
-			for (int k = 0; k < 784; k++) {
-				gradients[j].data[k] = 0;
-			}
+			memset(gradients[j].data, 0, 784);
 		}
 
 		for (int j = 0; j < num_training_examples; j++) {
@@ -61,28 +71,33 @@ void train(int iterations, float learn_rate, int num) {
 
 			for (int p = 0; p < 10; p++) {
 				// Compute this example's contribution to the gradient (for each weight)
+				struct Matrix* output = matrix_multiply(ensemble_weights[p], input_nodes);
+				float val = 1 - expectation[p] * output->data[0];
+				free_matrix(output);
 				for (int k = 0; k < 784; k++) {
-					// Hinge loss: max(0, 1 - y_i * o_i) for o_i = w^T * x_i
-					struct Matrix* output = matrix_multiply(ensemble_weights[p], input_nodes);
-					float val = 1 - expectation[p] * output->data[0];
-					val = val < 0 ? 0 : val;
-					// Add this example's impact to gradient update tally
-					gradients[p].data[k] += val;
-
-					free_matrix(output);
+					// Hinge loss: sum from j=1 to num_examples of max(0, 1 - y_j * w^T * x_j)
+					// Derivative w.r.t weight k: sum from j=1 to num_examples of { 0 if 1 <= y_j * w^T * x_j OR -y_j * x_jk otherwise }
+					if (val < 1) {
+						gradients[p].data[k] += -expectation[p] * input_nodes.data[k];
+					}
 				}
 			}
 		}
 
+		// printf("Gradient norms after iteration %d:\n", i);
 		// Update weights for each model
-		for (int i = 0; i < 10; i++) {
-			matrix_scale(&gradients[i], -1 * learn_rate);
-			matrix_add(&ensemble_weights[i], &gradients[i]);
+		for (int j = 0; j < 10; j++) {
+			// printf("\tModel %d: %.5f\n", j, norm(gradients[j]));
+			matrix_scale(&gradients[j], learn_rate);
+			matrix_add(&ensemble_weights[j], &gradients[j]);
 		}
+		
+		// End of the iteration, go back to the start of the training data file
+		rewind(training_data.file);
 	}
 
 	for (int i = 0; i < 10; i++) {
-		filepath[23] = i + '0';
+		filepath[FILEPATH_X_OFFSET] = i + '0';
 		write_csv_contents(filepath, ensemble_weights[i].data, 1, 784);
 		free_matrix_data(&ensemble_weights[i]);
 		free_matrix_data(&gradients[i]);
@@ -114,14 +129,10 @@ int main(int argc, char** argv) {
 		}
 	} else if (strncmp(argv[1], "train", 5) == 0) {
 		if (argc < 4) {
-			printf("Please supply a number of iterations and a learn rate, usage:\n\ttrain <iterations> <learn_rate> [<output=1>]\n");
+			printf("Please supply a number of iterations and a learn rate, usage:\n\ttrain <iterations> <learn_rate>\n");
 			exit(1);
 		}
-		if (argc < 5) {
-			train(atoi(argv[2]), atof(argv[3]), 1);
-		} else {
-			train(atoi(argv[2]), atof(argv[3]), atoi(argv[4]));
-		}
+		train(atoi(argv[2]), atof(argv[3]));
 	}else if (strncmp(argv[1], "init", 4) == 0) {
 		init();
 	} else {
